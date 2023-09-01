6233f1ced805ed97311e6a7acc2e71b1ad11f526
optimm <ayushsaxena823@gmail.com>
2023-09-01 15:22:09 +0530

    backend setup

---
+++ backend/config/index.js
@@ -0,0 +1,6 @@
+const ApplicationConfig = {
+  port: process.env.PORT,
+  frontendUrl: process.env.FRONTEND_URL,
+};
+
+module.exports = ApplicationConfig;
---
+++ backend/server.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+require("express-async-errors");
+
+//config
+const ApplicationConfig = require("./config");
+const config = require("./config");
+
+//imports********************
+const express = require("express");
+const app = express();
+const port = config.port;
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+//db
+const connectDb = require("./db/connect");
+
+//importing routers
+
+// error handler middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+//*******************************
+
+//middleware
+app.use(
+  cors({
+    credentials: true,
+    origin: `${config.frontendUrl}`,
+    sameSite: "none",
+  })
+);
+app.use(express.json({ limit: "4mb" }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+// error handler
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDb();
+    console.log("Connected to Database...\n");
+
+    app.listen(port, () => {
+      console.log(`Backend Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
