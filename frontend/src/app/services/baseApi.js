1bb9458b6e4e8244d6b6a4a7e18dbc4bd20ca3c7
optimm <ayushsaxena823@gmail.com>
2023-09-02 15:03:19 +0530

    frontend redux setup

---
+++ frontend/src/app/services/baseApi.js
@@ -0,0 +1,12 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { ApplicationConfig } from "../../config";
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${ApplicationConfig.backendUrl}/`,
+    credentials: "include",
+  }),
+  tagTypes: [],
+  endpoints: () => ({}),
+});
---
+++ frontend/src/config/index.js
@@ -0,0 +1,3 @@
+export const ApplicationConfig = {
+  backendUrl: import.meta.env.REACT_APP_BACKEND_URL,
+};
